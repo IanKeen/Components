4b347f948aba1a311d7a6eccb06ce47c411fad2a
Ian Keen <iankeen82@gmail.com>
2019-04-02 13:49:26 -0700

    Update DynamicProperties.swift

--- Components.playground/Sources/DynamicProperties/DynamicProperties.swift
+++ Components.playground/Sources/DynamicProperties/DynamicProperties.swift
@@ -1,6 +1,6 @@
 import ObjectiveC
 
-public protocol DynamicProperties: class {
+public protocol DynamicProperties: AnyObject {
     subscript<T>(dynamic key: String) -> T? { get set }
 }
 
@@ -9,10 +9,20 @@ private extension String {
         return UnsafeRawPointer(bitPattern: hashValue)!
     }
 }
-public extension DynamicProperties {
+extension DynamicProperties {
     public subscript<T>(dynamic key: String) -> T? {
         get { return objc_getAssociatedObject(self, key.unsafePointer) as? T }
         set { objc_setAssociatedObject(self, key.unsafePointer, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC) }
+    }
+    
+    public subscript<T>(dynamic key: String, or `default`: @autoclosure () -> T) -> T {
+        if let existing: T = self[dynamic: key] {
+            return existing
+        } else {
+            let value = `default`()
+            self[dynamic: key] = value
+            return value
+        }
     }
 }
 
